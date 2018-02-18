2dea31140a1fa708919dc8c388cb0c46d6a52bdd
chihiro <16201chihiro@gmail.com>
2018-02-18 13:39:31 +0900

    TalkRoomViewControllerのUI作成

--- messageApp/TalkRoomViewController.swift
+++ messageApp/TalkRoomViewController.swift
@@ -9,20 +9,59 @@
 import UIKit
 
 class TalkRoomViewController: UIViewController {
+    
+    var myX: Int = 250                        //自分のフキダシのX座標
+    var myY: Int = 50                         //自分のフキダシのY座標
+    @IBOutlet var scrollView: UIScrollView!   //フキダシを表示するScrollView
+    private var myTalkImageView: UIImageView! //自分のフキダシのImageView
+    private var myTalkLabel: UILabel!         //自分のフキダシのLabel
+    
+    @IBOutlet var textField: UITextField!     //コメントを入力するTextField
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func pushSend() {
+    @IBAction func pushSend() { //送信button
         
+    //フキダシについて
+        // myTalkImageViewを作成
+        myTalkImageView = UIImageView(frame: CGRect(x: myX, y: myY, width: 250, height: 30))
+        
+        // 表示する画像を設定
+        let myTalkImage = UIImage(named: "myTalk.png")
+        
+        //画像をmyTalkImageに設定
+        myTalkImageView.image = myTalkImage
+        
+        //myTalkImageViewの表示位置を設定
+        myTalkImageView.layer.position =  CGPoint(x: myX, y: myY)
+        
+        //myTalkImageViewをViewに追加
+        self.scrollView.addSubview(myTalkImageView)
+        
+    //フキダシに表示するmyTalkLabelについて
+        //myTalkLabelを作成
+        myTalkLabel = UILabel(frame: CGRect(x: myX + 5, y: myY, width: 250, height: 30))
+        
+        //myTalkLabelの表示位置を設定
+        myTalkLabel.layer.position = CGPoint(x: myX + 5, y: myY)
+        
+        //myTalkLabelをscrollViewに追加
+        self.scrollView.addSubview(myTalkLabel)
+        
+        //textFieldの文字をmyTalkLabelに表示
+        myTalkLabel.text = textField.text
+        
+        //textFieldを空白にする
+        textField.text = ""
+        
+        //次のフキダシのためにY座標を調整
+        myY += 50
     }
     
 
